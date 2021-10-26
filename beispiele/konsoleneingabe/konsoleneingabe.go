94649365f2ab8ade916a827be6d0f0e18ad174f2
Reiner Hüchting <46283923+ReinerHuechting@users.noreply.github.com>
2021-10-26 13:52:48 +0000

    Scanner-Beispiel

--- beispiele/konsoleneingabe/konsoleneingabe.go
+++ beispiele/konsoleneingabe/konsoleneingabe.go
@@ -1,7 +1,20 @@
 package main
 
 import "fmt"
+import "bufio" //buffered input/output
+import "os"    // operating system
 
 func main() {
   
+  // Erste Möglichkeit etwas einzugeben
+  scanner := bufio.NewScanner(os.Stdin) //standard input
+  fmt.Println("Bitte etwas eingeben: ")
+  scanner.Scan()
+  fmt.Println("Sie haben", scanner.Text(), "eingegeben.")
+
+// Zweite Möglichkeit etwas einzugeben
+  fmt.Println("Bitte noch etwas eingeben: ")
+  var input string
+  fmt.Scanln(&input)
+  fmt.Println("Sie haben", input, "eingegeben.")
 }
